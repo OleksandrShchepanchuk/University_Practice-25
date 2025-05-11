1f88865d50921dbdf099dbafc2477d35ddcfeaf3
Vhatever <fserrorka@gmail.com>
2025-05-11 23:44:29 +0300

    api and redux for movies and sessions

--- university-practice/front-end/src/App.tsx
+++ university-practice/front-end/src/App.tsx
@@ -1,8 +1,6 @@
 // src/App.tsx
 import { Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./testPages/LoginPage";
-import MoviesPage from "./testPages/MoviesPage";
-import SessionsPage from "./testPages/SessionsPage";
 
 const App = () => {
     return (
@@ -11,16 +9,10 @@ const App = () => {
                 <Link to="/" style={{ marginRight: 10 }}>
                     Login
                 </Link>
-                <Link to="/movies" style={{ marginRight: 10 }}>
-                    Movies
-                </Link>
-                <Link to="/sessions">Sessions</Link>
             </nav>
 
             <Routes>
                 <Route path="/" element={<LoginPage />} />
-                <Route path="/movies" element={<MoviesPage />} />
-                <Route path="/sessions" element={<SessionsPage />} />
             </Routes>
         </div>
     );
